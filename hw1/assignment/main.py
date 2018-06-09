75272a5d0206e9ca972569e0fcde4d3ab4a0587b
pholland08 <bulldog105@gmail.com>
2018-06-08 21:27:13 -0500

    Completed functions for assignment and added test case for multiples_of_5.

--- hw1/assignment/main.py
+++ hw1/assignment/main.py
@@ -1,6 +1,114 @@
+"""
+Phillip Holland
+COSC 5340
+Summer 2018
+Assignment 1
+"""
+
+import turtle
+
 
 def main():
-    return True
+    sven_main()
+    ole_main()
+    drawRectangle(turtle.Turtle(), 5, 10)
+    multiples_of_5()
+    bonus_turtle(15)
+
+
+def sven_main():
+
+    # Create a turtle called sven.
+    sven = turtle.Turtle()
+
+    # Now tell sven to go forward 10.
+    sven.forward(10)
+
+    # What is sven's position now?
+    print(sven.position())
+
+
+def ole_main():
+    # Create a turtle called Ole.
+    ole = turtle.Turtle()
+
+    # Tell Ole to turn right 45 degrees and go forward 50.
+    ole.right(45)
+    ole.forward(50)
+    print(ole.position(), ole.heading())
+
+
+def drawRectangle(myTurtle: turtle, width: int, height: int):
+    """
+    Create a new function called drawRectangle that takes three parameters: myTurtle,
+                                                                            width,
+                                                                            and height.
+    """
+
+    myTurtle.forward(width)
+    myTurtle.left(90)
+    myTurtle.forward(height)
+    myTurtle.left(90)
+    myTurtle.forward(width)
+    myTurtle.left(90)
+    myTurtle.forward(height)
+
+
+def multiples_of_5():
+    # Use the range function to create a sequence of the multiples of 5 up to 50
+    return range(5, 50, 5)
+
+
+def bonus_turtle(max_x: int):
+    # Bonus question: Use the turtle to plot the function y = x^2.
+
+    max_y = max_x*max_x
+
+    quad1_turtle = turtle.Turtle()
+    quad2_turtle = turtle.Turtle()
+
+    draw_axes(max_y, max_y)
+
+    quad1_turtle.setheading(90)
+    quad2_turtle.setheading(90)
+
+    # Draw parabola
+    for x in range(max_x):
+        y = x*x
+
+        quad1_turtle.goto(x, y)
+        quad2_turtle.goto(-x, y)
+
+
+def draw_axes(x_distance: int, y_distance: int):
+    draw_y_axis(y_distance)
+    draw_x_axis(x_distance)
+
+
+def draw_y_axis(distance: int):
+    north_turtle = turtle.Turtle()
+    south_turtle = turtle.Turtle()
+
+    # Set headings
+    north_turtle.setheading(90)
+    south_turtle.setheading(270)
+
+    # Draw lines
+    north_turtle.forward(distance)
+    south_turtle.forward(distance)
+
+
+def draw_x_axis(distance: int):
+    east_turtle = turtle.Turtle()
+    west_turtle = turtle.Turtle()
+
+    # Set headings
+    east_turtle.setheading(0)
+    west_turtle.setheading(180)
+
+    # Draw lines
+    east_turtle.forward(distance)
+    west_turtle.forward(distance)
 
 
 if __name__ == "__main__":
--- hw1/test_assignment/test_main.py
+++ hw1/test_assignment/test_main.py
@@ -1,35 +1,12 @@
 
 import unittest
-from hw1.assignment import main as other
-
-
-'''class SimpleTestCase(unittest.TestCase):
-
-	def setUp(self):
-		"""Call before every test case."""
-		self.foo = Foo()
-		self.file = open( "blah", "r" )
-
-	def tearDown(self):
-		"""Call after every test case."""
-		self.file.close()
-
-	def testA(self):
-		"""Test case A. note that all test method names must begin with 'test.'"""
-		assert foo.bar() == 543, "bar() not calculating values correctly"
-
-	def testB(self):
-		"""test case B"""
-		assert foo+foo == 34, "can't add Foo instances"
-
-	def testC(self):
-		"""test case C"""
-		assert foo.baz() == "blah", "baz() not returning blah correctly"'''
+from hw1.assignment import main
 
 
 class TestMain(unittest.TestCase):
-	def test_main(self):
-		self.assertTrue(other.main())
+	def test_multiples_of_5(self):
+		sequence = range(5, 50, 5)
+		self.assertEqual(sequence, main.multiples_of_5())
 
 
 if __name__ == "__main__":
