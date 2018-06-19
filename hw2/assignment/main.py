89300196f87c7b22f9fbe0eff194a2b9d64197b5
pholland08 <bulldog105@gmail.com>
2018-06-19 02:07:21 -0500

    Finished assignment 2 and added some tests.

--- hw2/assignment/main.py
+++ hw2/assignment/main.py
@@ -5,36 +5,72 @@ Summer 2018
 Assignment 2
 """
 
+import math
+import turtle
+
 
 def calculate_paycheck(pay_rate: float, hours_worked: float):
     """ Write a function that takes two parameters:  pay rate,
                                                     number of hours worked
         and returns the pay. Any hours over 40 are paid at time and a half. """
 
-    overtime = 0 if hours_worked <= 40 else hours_worked - 40
-    straight_time = 40 if hours_worked > 40 else hours_worked
+    overtime = 0 if hours_worked <= 40.0 else hours_worked - 40.0
+    straight_time = 40 if hours_worked > 40.0 else hours_worked
 
     standard_pay = straight_time * pay_rate
     overtime_pay = overtime * pay_rate * 1.5
     total_pay = standard_pay + overtime_pay
 
-    print(total_pay)
+    return total_pay
 
 
-def inCircle():
+def inCircle(point: (float, float), radius: float):
     """ Write a function inCircle that takes a point and a radius as a parameter.
         The function should return True if the point is inside the circle and False otherwise. """
 
+    hyp = math.hypot(point[0], point[1])
 
-def plot_all_points():
+    if radius < 0 or hyp > radius:
+        return False
+
+    return True
+
+
+def plot_all_points(radius: int):
     """ Modify the simulation to plot points in the entire circle.
         You will have to adjust the calculated value of pi accordingly.
         The program in Helper Programs->Chapter 2 is to help you.
         Test your output multiple times. """
 
+    turtle.tracer(0, 0)
+
+    ts = turtle.Screen()
+    ts.bgcolor("red")
+
+    t = turtle.Turtle()
+    t.color("green")
+    t.speed(10)
+    t.up()
+
+    for x in range(-2*radius, 2*radius, 1):
+        for y in range(-2*radius, 2*radius, 1):
+            if inCircle((x, y), radius):
+                plot_point(t, (x, y))
+
+    turtle.update()
+    t.hideturtle()
+    ts.exitonclick()
+
+
+def plot_point(t: turtle.Turtle, point: (float, float)):
+    t.goto(point[0], point[1])
+    t.dot(.01)
+
 
 def main():
     calculate_paycheck(10, 100)
+    inCircle((3, 4), 5)
+    plot_all_points(100)
 
 
 if __name__ == "__main__":
--- hw2/test_assignment/test_main.py
+++ hw2/test_assignment/test_main.py
@@ -1,12 +1,16 @@
 
 import unittest
+import turtle
 from hw2.assignment import main as other
 
 
 class TestMain(unittest.TestCase):
+	"""
+	calculate_paycheck tests
+	"""
 	def test_calculate_paycheck(self):
-		pay_rate: float = 10
-		hours_worked: float = 0
+		pay_rate = 10
+		hours_worked = 0
 
 		# Assert 0 (0 + 0)
 		self.assertEqual(float(0), other.calculate_paycheck(pay_rate, hours_worked))
@@ -22,6 +26,27 @@ class TestMain(unittest.TestCase):
 		# Assert 100 (400 + 900)
 		hours_worked = 100
 		self.assertEqual(float(1300), other.calculate_paycheck(pay_rate, hours_worked))
+
+	"""
+	inCircle tests
+	"""
+	def test_inCircle_NonNegativeRadius(self):
+		self.assertFalse(other.inCircle((-1, -4), -5))
+
+	def test_inCircle_CoordsNotGreaterThanRadius(self):
+		self.assertFalse(other.inCircle((0, 0), -1))
+
+	def test_inCircle_HypotenuseNotGreaterThanRadius(self):
+		self.assertFalse(other.inCircle((4, 4), 5))
+
+	def test_inCircle_PointOnRadius345(self):
+		self.assertTrue(other.inCircle((3, 4), 5))
+
+	def test_inCircle_PointInsideRadius125(self):
+		self.assertTrue(other.inCircle((1, 2), 5))
+
+	def test_inCircle_NegativePointOnRadius(self):
+		self.assertTrue(other.inCircle((-3, -4), 5))
 
 
 if __name__ == "__main__":
