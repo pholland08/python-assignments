5b11bc80daba57dd9c9c3364b10b57e702d49902
Phillip Holland <bulldog105@gmail.com>
2018-07-19 22:31:45 -0500

    Assignment 8 and tests.

--- hw8/assignment/main.py
+++ hw8/assignment/main.py
@@ -5,11 +5,29 @@ Summer 2018
 Assignment 7 Ch. 8
 """
 
+import re
+import time
 
-def createWordList():
+
+def createWordList() -> list:
 	"""
 	8.1 Write a createWordList function to create a list of words from the wordlist file.
 	"""
+	with open("../resources/wordlist.txt", "r") as word_file:
+		return word_file.readlines()
+
+
+def createWordSet() -> set:
+	return set(createWordList())
+
+
+def createWordDict() -> dict:
+	d = {}
+	with open("../resources/wordlist.txt", "r") as word_file:
+		for line in word_file.readlines():
+			d[line] = None
+
+	return d
 
 
 def ex_8_3():
@@ -22,6 +40,30 @@ def ex_8_3():
 		list/set/dictionary. You could extend this experiment to show how one of the data structures makes the rest of the
 		program run more quickly as well.
 	"""
+
+	# On my computer, the execution results are as follows:
+	#
+	# createWordList:  0.008999109268188477
+	# createWordSet:  0.010995626449584961
+	# createWordDict:  0.010996580123901367
+	#
+	# While createWordList has slightly faster performance in creation, any benefit is lost as soon as a single
+	# value lookup is performed. Not much difference is seen between createWordSet and createWordDict.
+
+	start = time.time()
+	createWordList()
+	finish = time.time()
+	print("createWordList: ", (finish - start))
+
+	start = time.time()
+	createWordSet()
+	finish = time.time()
+	print("createWordSet: ", (finish - start))
+
+	start = time.time()
+	createWordDict()
+	finish = time.time()
+	print("createWordDict: ", (finish - start))
 
 
 def ex_8_10():
@@ -38,54 +80,98 @@ def ex_8_10():
 	return l1  # The return is None
 
 
-def ex_8_11():
+def ex_8_11(t1: tuple, t2: tuple) -> bool:
 	"""
 	8.11 Write a compare function that accepts two frequency dictionary tuples as parameters and compares the tuples
 		using the second element.
 	"""
+	# Not sure what is being asked on this question.
+	return t1[1] > t2[1]
 
 
-def ex_8_15():
+def ex_8_15(words: list) -> str:
 	"""
 	8.15 Write a function that returns the most popular ending letter for words. Your program will take a list of
 		words, count the ending letter of each word and return the letter whose count is the largest.
 	"""
+	endings: dict = {}
+
+	for word in words:
+		ending = word[-1]
+
+		if endings.get(ending) is None:
+			endings[ending] = 0
+
+		endings[ending] = endings[ending] + 1
+
+	items = list(endings.items())
+	items.sort(key=lambda x: x[1], reverse=True)
+
+	return items[0][0]
 
 
-def ex_8_19():
+def ex_8_19(text: str) -> list:
 	"""
 	8.19 Write a regular expression pattern to match all words ending in "ing"
 	"""
 
+	return match_in_text(text, r"((\w|-)*ing)\b")
 
-def ex_8_20():
+
+def match_in_text(text: str, regex: str) -> list:
+	matches: list = []
+
+	if text:
+		words: list = text.split()
+
+		matches = match_from_list(words, regex)
+
+	return matches
+
+
+def match_from_list(words: list, regex: str) -> list:
+	matches: list = []
+
+	for word in words:
+		m = re.match(regex, word, re.IGNORECASE)
+		if m:
+			matches.append(m.group(1))
+
+	return matches
+
+
+def ex_8_20(text: str) -> list:
 	"""
 	8.20 Write a regular expression pattern to match all words with ss anywhere in the string.
 	"""
+	return match_in_text(text, r"(\w*ss\w*)\b")
 
 
-def ex_8_22():
+def ex_8_22(text: str) -> list:
 	"""
 	8.22 Write a regular expression pattern to match all the words that start with st.
 	"""
+	return match_in_text(text, r"\b(st\w*)\b")
 
 
-def ex_8_23():
+def ex_8_23(text: str) -> list:
 	"""
 	8.23 Write a regular expression to match all the four-letter words where the middle two letters are vowels.
 	"""
+	return match_in_text(text, r"\b([A-z][AEIOUaeiou]{2}[A-z])\b")
 
 
-def ex_8_24():
+def ex_8_24(url: str) -> str:
 	"""
 	8.24 Write a function that can extract the host name from a URL. The host name is the part of the URL that
 		comes after http:// but before the next /. Here is a simple one that matches hostnames that
 		consist of letters and the dot character.
 	"""
+	return match_in_text(url, r"\bhttps?://((\w|\.)+)\b")[0]
 
 
 def main():
-	pass
+	ex_8_3()
 
 
 if __name__ == "__main__":
--- hw8/test_assignment/test_main.py
+++ hw8/test_assignment/test_main.py
@@ -1,10 +1,143 @@
 
 import unittest
+import hw8.assignment.main as other
 
 
 class TestMain(unittest.TestCase):
-	def test_thing(self):
-		pass
+	def test_createWordList_return_list(self):
+		response: list = other.createWordList()
+		self.assertTrue(type(response) is list)
+
+	def test_ex_8_15_returns_str(self):
+		words = ["first", "second", "third", "fourth", "fifth"]
+		expected = type('h')
+		response = other.ex_8_15(words)
+
+		self.assertEqual(expected, type(response))
+
+	def test_ex_8_15_returns_one_letter(self):
+		words = ["first", "second", "third", "fourth", "fifth"]
+		expected = 1
+		response = other.ex_8_15(words)
+
+		self.assertEqual(expected, len(response))
+
+	def test_ex_8_15_returns_ending_letter(self):
+		words = ["first", "second", "third", "fourth", "fifth"]
+		expected = ['t', 'd', 'h']
+		response = other.ex_8_15(words)
+
+		self.assertTrue(response in expected)
+
+	def test_ex_8_15_returns_most_common_ending_letter(self):
+		words = ["first", "second", "third", "fourth"]
+		expected = 'd'
+		response = other.ex_8_15(words)
+
+		self.assertEqual(expected, response)
+
+	def test_ex_8_19_returns_list(self):
+		text = ""
+		expected = list(text)
+		response = other.ex_8_19(text)
+
+		self.assertEqual(type(expected), type(response))
+
+	def test_ex_8_19_matches_ing(self):
+		text: str = "ing"
+		expected = [text, ]
+		response = other.ex_8_19(text)
+
+		self.assertEqual(expected, response)
+
+	def test_ex_8_19_matches_multiple_ing(self):
+		text: str = "ing ing"
+		expected = ["ing", "ing"]
+		response = other.ex_8_19(text)
+
+		self.assertEqual(expected, response)
+
+	def test_ex_8_19_matches_something(self):
+		text: str = "something"
+		expected = [text, ]
+		response = other.ex_8_19(text)
+
+		self.assertEqual(expected, response)
+
+	def test_ex_8_19_doesnt_match_other_stuff(self):
+		text: str = "other stuff"
+		expected = []
+		response = other.ex_8_19(text)
+
+		self.assertEqual(expected, response)
+
+	def test_ex_8_19_doesnt_match_other_stuff(self):
+		text: str = "other stuff"
+		expected = []
+		response = other.ex_8_19(text)
+
+		self.assertEqual(expected, response)
+
+	def test_ex_8_19_matches_other_stuffing(self):
+		text: str = "other-stuffing"
+		expected = [text, ]
+		response = other.ex_8_19(text)
+
+		self.assertEqual(expected, response)
+
+	def test_ex_8_19_thanksgiving(self):
+		text: str = "For Thanksgiving dinner, we had turkey, gravy, stuffing, and special-something pie."
+		expected = ["Thanksgiving", "stuffing", "special-something"]
+		response = other.ex_8_19(text)
+
+		self.assertEqual(expected, response)
+
+	def test_ex_8_20_returns_emptylist_for_blank(self):
+		text: str = ""
+		response: list = other.ex_8_20(text)
+
+		self.assertTrue(type(response) is list)
+
+	def test_ex_8_20_matches_single_ss(self):
+		text: str = "ss"
+		response: list = other.ex_8_20(text)
+
+		self.assertTrue([text, ] == response)
+
+	def test_ex_8_20_matches_missing_only(self):
+		text: str = "not missing"
+		expected: list = ["missing", ]
+		response: list = other.ex_8_20(text)
+
+		self.assertEqual(expected, response)
+
+	def test_ex_8_20_matches_pass_hissing_only(self):
+		text: str = "As you pass, the cat will be hissing."
+		expected: list = ["pass", "hissing"]
+		response: list = other.ex_8_20(text)
+
+		self.assertEqual(expected, response)
+
+	def test_ex_8_22_matches(self):
+		text: str = "Stop after the first one."
+		expected: list = ["Stop", ]
+		response: list = other.ex_8_22(text)
+
+		self.assertEqual(expected, response)
+
+	def test_ex_8_23_matches(self):
+		text: str = "All your bases are belong to me."
+		expected: list = ["your", ]
+		response: list = other.ex_8_23(text)
+
+		self.assertEqual(expected, response)
+
+	def test_ex_8_24_matches(self):
+		text: str = "http://www.thedevneedscoffee.com?asdf"
+		expected: str = "www.thedevneedscoffee.com"
+		response: list = other.ex_8_24(text)
+
+		self.assertEqual(expected, response)
 
 
 if __name__ == "__main__":
